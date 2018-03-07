0247061f6997531bedfc4b40b4070066ea04609d
Taylor Stemple <taylorstemple@gmail.com>
2018-03-07 10:53:42 -0800

    post route continued

--- lab-taylor/routes/pic-router.js
+++ lab-taylor/routes/pic-router.js
@@ -12,7 +12,52 @@ const debug = require('debug')('instaclone:pic-router');
 const Pic = require('../model/pic.js');
 const Gallery = require('../model/gallery.js');
 const bearerAuth = require('../lib/bearer-auth.js');
+const picRouter = module.exports = Router();
 
 AWS.config.setPromisesDependency(require('bluebird'));
 
 const s3 = new AWS.S3();
+const dataDir = `${__dirname}../data`;
+const upload = multer({ dest: dataDir });
+
+function s3uploadProm(params) {
+  debug('s3uploadProm');
+  return new Promise((resolve, reject) => {
+    s3.upload(params, (err, s3data) => {
+      resolve(s3data);
+    });
+  });
+}
+
+picRouter.post('/api/gallery/:galleryId/pic', bearerAuth, upload.single('image'), function(req, res, next) {
+  debug('POST: /api/gallery/:galleryId/pic');
+
+  if(!req.file) {
+    return next(createError(400, 'file not found'));
+  }
+
+  if(!req.file.path) {
+    return next(createError(500, 'file not saved'));
+  }
+
+  let ext =path.extname(req.file.originalname);
+
+  let params = {
+    ACL: 'public-read',
+    Bucket: process.env.AWS_BUCKET,
+    Key: `${req.file.filename}${ext}`,
+    Body: fs.createReadStream(req.file.path),
+  };
+
+  Gallery.findById(req.params.galleryId)
+    .then( () => s3uploadProm(params))
+    .then( s3data => {
+      console.log('s3 res:', s3data);
+      del([`${dataDir}/*`]);
+
+      let picData = {
+        
+      }
+    })
+});
+      
